dbce3f1dc0a2a256c385c0095671ba183669cb5b
juandpt03 <juandpt03@gmail.com>
2024-06-24 18:36:26 -0500

    refactor: update getSmsManager() to handle different Android versions

--- android/src/main/kotlin/com/shounakmulay/telephony/sms/SmsController.kt
+++ android/src/main/kotlin/com/shounakmulay/telephony/sms/SmsController.kt
@@ -140,10 +140,19 @@ class SmsController(private val context: Context) {
     }
 
   
-    private fun getSmsManager(): SmsManager {
+     private fun getSmsManager(): SmsManager {
         val subscriptionId = SmsManager.getDefaultSmsSubscriptionId()
-        val smsManager = SmsManager.getDefault()
-            ?: throw RuntimeException("Flutter Telephony: Error getting SmsManager")
+        val smsManager : SmsManager?
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            smsManager = getSystemService(context, SmsManager::class.java)
+        } else {
+            smsManager = SmsManager.getDefault()
+        }
+
+        if(smsManager == null) {
+            throw RuntimeException("Flutter Telephony: Error getting SmsManager")
+        }
+
         if (subscriptionId != SubscriptionManager.INVALID_SUBSCRIPTION_ID) {
             return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                 smsManager.createForSubscriptionId(subscriptionId)
